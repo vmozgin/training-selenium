af5acbea34b1f8c8e784af36faaaa1b9cea2be41
vladimirmozgin <v.mosgin@gmail.com>
2022-02-17 23:28:36 +0300

    add CheckContentStickersTest

---
+++ src/test/java/selenium/CheckContentStickersTest.java
@@ -0,0 +1,43 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class CheckContentStickersTest {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void start() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void checkContentStickersTest() {
+        driver.get("http://localhost/litecart/");
+        List<WebElement> productsList = driver.findElements(By.xpath("//*[@class='product column shadow hover-light']"));
+
+        for (WebElement product : productsList) {
+            List<WebElement> stickersList = product.findElements(By.className("sticker"));
+            Assert.assertTrue(isStickerListEqualsOne(stickersList));
+        }
+    }
+
+    @AfterClass
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+    private boolean isStickerListEqualsOne(List<WebElement> stickers) {
+        return stickers.size() == 1;
+    }
+}
